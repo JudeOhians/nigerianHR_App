3e06242aecc846b1307261746f69d56f954e64b3
JudeOhians <judeohiani@gmail.com>
2023-09-13 13:31:11 +0100

    Update

--- Nigerian HR App.sql
+++ Nigerian HR App.sql
@@ -2,14 +2,14 @@ CREATE TABLE "Employee" (
   "Employee_ID" integer PRIMARY KEY,
   "First_Name" varchar,
   "Last_Name" varchar,
+  "Employment_Type" varchar,
   "Job_Position_ID" integer,
-  "Employment_Type" varchar
+  "Deparment_ID" integer
 );
 
 CREATE TABLE "Department" (
   "Deparment_ID" integer PRIMARY KEY,
-  "Dewpartment_Name" varchar,
-  "Employee" varchar
+  "Dewpartment_Name" varchar
 );
 
 CREATE TABLE "Job_Position" (
@@ -19,7 +19,7 @@ CREATE TABLE "Job_Position" (
 
 CREATE TABLE "Payroll" (
   "Payroll_ID" integer PRIMARY KEY,
-  "Employee_ID" varchar,
+  "Employee_ID" integer,
   "Monthly_salary" varchar,
   "Hourly_rate" varchar,
   "created_at" timestamp
@@ -27,6 +27,7 @@ CREATE TABLE "Payroll" (
 
 CREATE TABLE "Leave_Request" (
   "Leave_Request_ID" integer PRIMARY KEY,
+  "Employee_ID" integer,
   "Request_Date" timestamp,
   "Start_Date" timestamp,
   "End_Date" timestamp,
@@ -41,40 +42,21 @@ CREATE TABLE "Training_Session" (
 
 CREATE TABLE "Attendance_Record" (
   "Attendance_Record_ID" integer PRIMARY KEY,
-  "Employee_ID" varchar,
+  "Employee_ID" integer,
+  "Training_Session_ID" integer,
   "Attendance_Date" timestamp
 );
 
-ALTER TABLE "Department" ADD FOREIGN KEY ("Employee") REFERENCES "Employee" ("Employee_ID");
-
 ALTER TABLE "Employee" ADD FOREIGN KEY ("Job_Position_ID") REFERENCES "Job_Position" ("Job_Position_ID");
 
-ALTER TABLE "Leave_Request" ADD FOREIGN KEY ("Leave_Request_ID") REFERENCES "Employee" ("Employee_ID");
-
-CREATE TABLE "Attendance_Record_Training_Session" (
-  "Attendance_Record_Attendance_Record_ID" integer,
-  "Training_Session_Training_Session_ID" integer,
-  PRIMARY KEY ("Attendance_Record_Attendance_Record_ID", "Training_Session_Training_Session_ID")
-);
-
-ALTER TABLE "Attendance_Record_Training_Session" ADD FOREIGN KEY ("Attendance_Record_Attendance_Record_ID") REFERENCES "Attendance_Record" ("Attendance_Record_ID");
-
-ALTER TABLE "Attendance_Record_Training_Session" ADD FOREIGN KEY ("Training_Session_Training_Session_ID") REFERENCES "Training_Session" ("Training_Session_ID");
-
-
-ALTER TABLE "Attendance_Record" ADD FOREIGN KEY ("Employee_ID") REFERENCES "Employee" ("Employee_ID");
-
-CREATE TABLE "Employee_Attendance_Record" (
-  "Employee_Employee_ID" integer,
-  "Attendance_Record_Attendance_Record_ID" integer,
-  PRIMARY KEY ("Employee_Employee_ID", "Attendance_Record_Attendance_Record_ID")
-);
-
-ALTER TABLE "Employee_Attendance_Record" ADD FOREIGN KEY ("Employee_Employee_ID") REFERENCES "Employee" ("Employee_ID");
-
-ALTER TABLE "Employee_Attendance_Record" ADD FOREIGN KEY ("Attendance_Record_Attendance_Record_ID") REFERENCES "Attendance_Record" ("Attendance_Record_ID");
-
+ALTER TABLE "Employee" ADD FOREIGN KEY ("Deparment_ID") REFERENCES "Department" ("Deparment_ID");
 
 ALTER TABLE "Payroll" ADD FOREIGN KEY ("Hourly_rate") REFERENCES "Employee" ("Employment_Type");
 
 ALTER TABLE "Payroll" ADD FOREIGN KEY ("Monthly_salary") REFERENCES "Employee" ("Employment_Type");
+
+ALTER TABLE "Leave_Request" ADD FOREIGN KEY ("Employee_ID") REFERENCES "Employee" ("Employee_ID");
+
+ALTER TABLE "Attendance_Record" ADD FOREIGN KEY ("Training_Session_ID") REFERENCES "Training_Session" ("Training_Session_ID");
+
+ALTER TABLE "Attendance_Record" ADD FOREIGN KEY ("Employee_ID") REFERENCES "Employee" ("Employee_ID");
